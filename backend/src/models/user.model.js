205a11d96cea1a656c5d6a25ad1933f302bb8dc8
nayan9617 <nayanp4925@gmail.com>
2025-08-20 13:15:37 +0530

    methods for userSchema in order to generate tokens

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
@@ -41,5 +42,31 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.hash(password, this.password);
 };
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+           _id: this._id,
+           email: this.email,
+           name: this.name,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+return jwt.sign(
+        {
+           _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema);
