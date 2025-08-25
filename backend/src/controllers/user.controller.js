1782945dfbfb451d0cee7c8e37b00c80815bfdc9
nayan9617 <nayanp4925@gmail.com>
2025-08-26 05:23:36 +0530

    accepting user registrations

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,14 +1,15 @@
-import { User } from "../models/user.model";
+import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken"
 // import mongoose from "mongoose";
 
 const registerUser = async (req, res) => {
-    const { name, email, password, role } = req.body;
+  try{
+      const { name, email, password } = req.body;
 
     if(
-        [name, email, password, role].some(field => field?.trim() == "")
+        [name, email, password].some(field => field?.trim() == "")
     ){
-        res.status(400).json({message: "All fields are required!"});
+       return res.status(400).json({message: "All fields are required!"});
     }
 
     const existingUser = await User.findOne(
@@ -17,21 +18,26 @@ const registerUser = async (req, res) => {
     }
     )
     if(existingUser){
-     res.status(409).json({ message: "User already exists, please login!" });   
+     return res.status(409).json({ message: "User already exists, please login!" });   
     }
     const user = await User.create({
-        name: name,
-        email: email,
-        password: password,
-        role: role,
+        name,
+        email,
+        password,
     })
     const createdUser = await User.findById(user?._id).select("-password -refreshToken");
 
     if(!createdUser){
-        res.status(500).json({ message: "Something went wrong while registering the user!" })
+       return res.status(500).json({ message: "Something went wrong while registering the user!" })
     }
     
     return res.status(201).json(
    { message: "User registered successfully!" }
 )
+  } catch(error){
+    console.error("Register Error:", error);
+    return res.status(500).json({ message: "Server error", error: error.message });
+  }
 }
+
+export { registerUser };
