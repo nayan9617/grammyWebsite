658bb11d4c5d16b938c74bdda0de321f2629ed3e
nayan9617 <nayanp4925@gmail.com>
2025-08-26 06:01:00 +0530

    logic for user login

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser , loginUser} from "../controllers/user.controller.js";
 
 const userRoutes = express.Router();
 
 userRoutes.post("/register", registerUser);
+userRoutes.post("/login", loginUser);
 
 export default userRoutes;
