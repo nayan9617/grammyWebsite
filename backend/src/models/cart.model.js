01a87dc25cf393cb4e6344d68f7494b70c7445c0
nayan9617 <nayanp4925@gmail.com>
2025-09-23 15:08:23 +0530

    added cart schema

---
+++ backend/src/models/cart.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+const cartSchema=new mongoose.Schema({
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    product:{
+        type:Schema.Types.ObjectId,
+        ref:"Product",
+        required:true
+    },
+    quantity:{
+        type:Number,
+        default:1,
+    }
+}, {versionKey:false})
+
+export const Cart = mongoose.model("Cart", cartSchema);
