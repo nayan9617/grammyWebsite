4405c98f4b025ddd4c7c0a781696c8831d4fa05b
nayan9617 <nayanp4925@gmail.com>
2025-09-23 14:37:26 +0530

    added product schema

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const productSchema= new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    category:{
+        type:Schema.Types.ObjectId,
+        ref:"Category",
+        required:true
+    },
+    brand:{
+        type:Schema.Types.ObjectId,
+        ref:"Brand",
+        required:true
+    },
+    stockQuantity:{
+        type:Number,
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        required:true
+    },
+    images:{
+        type:[String],
+        required:true
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    }
+},{timestamps:true,versionKey:false})
+
+export const Product = mongoose.model('Product', productSchema);
