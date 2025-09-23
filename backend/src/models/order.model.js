4e2881241e01785db390c8f25ff1d1dfb447a4df
nayan9617 <nayanp4925@gmail.com>
2025-09-23 15:40:29 +0530

    added order schema

---
+++ backend/src/models/order.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+
+const orderSchema=new mongoose.Schema({
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    item:{
+        type:[Schema.Types.Mixed],
+        required:true
+    },
+    address:{
+        type:[Schema.Types.Mixed],
+        required:true
+    },
+    status:{
+        type:String,
+        enum:['Pending','Dispatched','Out for delivery','Cancelled'],
+        default:'Pending'
+    },
+    paymentMode:{
+        type:String,
+        enum:['COD','UPI','CARD'],
+        required:true
+    },
+    total:{
+        type:Number,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+},{versionKey:false})
+
+export const Order = mongoose.model("Order", orderSchema)
